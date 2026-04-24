be333c3973bb48725159d11118b82357da74bf3a
Sundar Nambuvel <sunprema@gmail.com>
2026-04-24 12:59:18 -0500

    landing page added

--- lib/genie_web/controllers/page_controller.ex
+++ lib/genie_web/controllers/page_controller.ex
@@ -1,6 +1,9 @@
 defmodule GenieWeb.PageController do
   use GenieWeb, :controller
 
+  plug :put_root_layout, html: {GenieWeb.Layouts, :landing}
+  plug :put_layout, false
+
   def home(conn, _params) do
     render(conn, :home)
   end
